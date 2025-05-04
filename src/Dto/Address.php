9b0c0b714a4499e30056dc40e95835afd952c72e
Timor9 <141584263+Timor9@users.noreply.github.com>
2025-05-04 13:04:08 +0200

    Update Address.php

--- src/Dto/Address.php
+++ src/Dto/Address.php
@@ -5,11 +5,11 @@ namespace MullIo\Core\Dto;
 class Address implements \Stringable
 {
     public function __construct(
-        public string $street = '',
-        public string $houseNumber = '',
-        public string $zip = '',
-        public string $city = '',
-        public string $country = '',
+        public string $street = 'Pumpernudl',
+        public string $houseNumber = '3',
+        public string $zip = '84104',
+        public string $city = 'Rudelzhausen',
+        public string $country = 'Deutschland',
     )
     {
     }
